793193c4bfcb64108ac1c898e4d9e0bba9042fc2
Tran Vo <86919522+TranHap@users.noreply.github.com>
2023-07-09 21:18:10 +0700

    Update SectionWrapper.jsx

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -7,16 +7,13 @@ const SectionWrapper = (Component) =>
   function HOC() {
     return (
         <motion.section
-            // variants={staggerContainer()}
             initial="hidden"
             whileInView="show"
             viewport={{once:false, amount:0.1}}
             className={`${styles.padding} 
                 max-w-10xl mx-auto h-full`}
         >
-            {/* <span className='hash-span' id={idName}>
-                &nbsp;
-            </span> */}
+          
             <Component />
         </motion.section>
     )
